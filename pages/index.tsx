6b6fa2f3375556177eb27da228e695d9f7c380da
DinosDev <erick@bilhalba.com.br>
2022-02-25 18:46:30 -0300

    feat: Import Header Component in Index Page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,10 @@
+import React from "react"
+import Header from "../src/components/Header"
+
 export default function IndexPage() {
-    return (<div>Nystron</div>)
+    return (
+        <React.Fragment>
+            <Header />
+        </React.Fragment>
+    )
 }
