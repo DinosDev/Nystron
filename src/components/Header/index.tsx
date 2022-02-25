5129d87dc751bf9c86d01eb533f22d1dcf2b166b
DinosDev <erick@bilhalba.com.br>
2022-02-25 20:31:18 -0300

    feat: add CSS To Header Container

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,27 @@
+import styled from "styled-components"
+
+const HeaderContainer = styled.header`
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    padding: 10px;
+    border-bottom: 1px solid black;
+    background-color: #d1812f;
+
+    * {
+        color: #00548b;
+        width: max-content;
+        font-size: 20px;
+    }
+
+    @media (max-width: 830px) {
+        flex-flow: column wrap; 
+        justify-content: space-around;
+    }
+`
+
 export default function Header() {
-    return (<header></header>)
+    return (
+        <HeaderContainer> </HeaderContainer>
+    )
 }
