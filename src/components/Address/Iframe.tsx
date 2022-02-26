53ce03fb8aada854b00de98861d8c3c16898b58f
DinosDev <erick@bilhalba.com.br>
2022-02-26 15:15:25 -0300

    feat: Add Iframe To Address

---
+++ src/components/Address/Iframe.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Iframe = styled.iframe`
+    width: 90%;
+    height: 450px;
+    border-width: 1px;
+    border-style: solid;
+    border-color: black;
+    border-image: initial;
+`
+
+export default Iframe;
--- src/components/Address/index.tsx
+++ src/components/Address/index.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components"
 import Title from "./Title"
+import Iframe from "./Iframe"
 
 const AddressContainer = styled.div`
     display: flex;
@@ -8,10 +9,13 @@ const AddressContainer = styled.div`
     flex-flow: row wrap;
 `
 
+const IframeSrc = "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13766.673301382876!2d-56.4538955!3d-30.3887748!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x54d29733a91d9b03!2sMadeireira%20Nystron!5e0!3m2!1spt-BR!2sbr!4v1628554787566!5m2!1spt-BR!2sbr"
+
 export default function Address() {
     return (
         <AddressContainer>
             <Title>Nosso Endereço</Title>
+            <Iframe src={IframeSrc} />
         </AddressContainer>
     )
 }
