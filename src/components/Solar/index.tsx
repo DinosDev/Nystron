b373259dda8852851f2a3e348359149942cec623
DinosDev <erick@bilhalba.com.br>
2022-02-26 15:35:59 -0300

    feat: Import Solar Component In Index Page

---
+++ src/components/Solar/index.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const SolarContainer = styled.div`
+    text-align: center;
+`
+
+export default function Solar() {
+    return (
+        <SolarContainer></SolarContainer>
+    );
+}
