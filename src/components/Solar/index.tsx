5fe8b2d3f763f941432eccccf8d4a8fc9f2455b7
DinosDev <erick@bilhalba.com.br>
2022-02-26 15:56:49 -0300

    feat: Add Text To Solar Component

--- src/components/Solar/index.tsx
+++ src/components/Solar/index.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import Title from "./Title";
+import Text from "./Text";
 
 const SolarContainer = styled.div`
     text-align: center;
@@ -9,7 +10,7 @@ export default function Solar() {
     return (
         <SolarContainer>
             <Title>Nystron Solar</Title>
-
+            <Text />
         </SolarContainer>
     );
 }
