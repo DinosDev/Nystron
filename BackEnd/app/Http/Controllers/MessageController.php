f76ca4a9636f86697f57e67884b6549c755fd980
DinosDev <erick@bilhalba.com.br>
2022-03-22 19:55:17 -0300

    feat: Add Validation To Messages Route

--- BackEnd/app/Http/Controllers/MessageController.php
+++ BackEnd/app/Http/Controllers/MessageController.php
@@ -8,7 +8,13 @@ class MessageController extends Controller
 {
     public function store(Request $request)
     {
-        $data = $request->only("name", "lastName", "telephone", "email", "message");
+        $data = $this->validate($request, [
+            "name" => ["required", "max:50"],
+            "lastName" => ["required", "max:100"],
+            "telephone" => ["required", "max:21", 'regex:#^\+([0-9]{2,3})((\s\([0-9]{2}\)\s)|(\s))([0-9]{4,5})\s-\s([0-9]{4})$#'],
+            "email" => ["required", "max:100", "email"],
+            "message" => ["required", "max:1000"]
+        ]);
 
         return $data;
     }
