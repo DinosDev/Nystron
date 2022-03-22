1d9f4b820e57477dffa1bf598d8f927e47a72c4c
DinosDev <erick@bilhalba.com.br>
2022-03-22 19:53:50 -0300

    fix: Add Fillable Values To Messages Model

--- BackEnd/app/Models/MessageModel.php
+++ BackEnd/app/Models/MessageModel.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class MessageModel extends Model
 {
     protected $table = 'messages';
+
+    protected $fillable = [
+        "name", "lastName", "telephone", "email", "message"
+    ];
 }
