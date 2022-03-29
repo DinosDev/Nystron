64b6acc3510bd0a354af1ec23fa9af909e73fa85
DinosDev <erick@bilhalba.com.br>
2022-03-29 18:44:52 -0300

    feat(Back End): Add A Function To Message Model
    All 'Create Value" Function To Message Model. The Function Will Try To Create A Value In Database.
    If A Exception Appears, He Will Return Error. Else, It Will Return A Success

--- BackEnd/app/Models/MessageModel.php
+++ BackEnd/app/Models/MessageModel.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\QueryException;
+use Illuminate\Support\Facades\DB;
 
 class MessageModel extends Model
 {
@@ -11,4 +13,28 @@ class MessageModel extends Model
     protected $fillable = [
         "name", "lastName", "telephone", "email", "message"
     ];
+
+    static public function createValue(array $Values): array
+    {
+        DB::beginTransaction();
+
+        try {
+            self::create($Values);
+            DB::commit();
+
+            return [
+                "Status Code" => "201",
+                "Type" => "Success",
+                "Message" => "Mensagem Enviada."
+            ];
+        } catch (QueryException) {
+            DB::rollBack();
+
+            return [
+                "Status Code" => "500",
+                "Type" => "Error",
+                "Message" => "Erro ao enviar mensagem."
+            ];
+        }
+    }
 }
