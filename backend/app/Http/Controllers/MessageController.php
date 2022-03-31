fc97d3259f7a43fa5cd4c56149dffa2b1e12cc3d
DinosDev <erick@bilhalba.com.br>
2022-03-31 00:57:20 -0300

    feat(Back End): Send Mail On Message Route

--- backend/app/Http/Controllers/MessageController.php
+++ backend/app/Http/Controllers/MessageController.php
@@ -4,12 +4,23 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\MessageRequest;
 use App\Models\MessageModel;
+use App\Mail\NewMessage;
 
 class MessageController extends Controller
 {
     public function store(MessageRequest $request)
     {
         $RequestValues = $request->only(['name', 'lastName', 'telephone', 'email', 'message']);
+
+        $Mail = NewMessage::SendMail($RequestValues);
+
+        if ($Mail !== NULL) {
+            return response()->json([
+                "Status Code" => "500",
+                "Type" => "Error",
+                "Message" => "Erro ao enviar mensagem."
+            ], 500);
+        }
 
         return MessageModel::createValue($RequestValues);
     }
