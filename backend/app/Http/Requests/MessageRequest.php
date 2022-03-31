2fa2c912c9f3deb896e8d443e3c13156e90a542d
DinosDev <erick@bilhalba.com.br>
2022-03-31 18:27:12 -0300

    feat(Back End): Change Back End Error Messages Language

--- backend/app/Http/Requests/MessageRequest.php
+++ backend/app/Http/Requests/MessageRequest.php
@@ -19,7 +19,7 @@ class MessageRequest extends FormRequest
         return [
             "name" => ["required", "max:50"],
             "lastName" => ["required", "max:100"],
-            "telephone" => ["required", "max:21", 'regex:#^\+([0-9]{2,3})((\s\([0-9]{2}\)\s)|(\s))([0-9]{4,5})\s-\s([0-9]{4})$#'],
+            "telephone" => ["required", "max:21", "regex:#^\+([0-9]{2,3})((\s\([0-9]{2}\)\s)|(\s))([0-9]{4,5})\s-\s([0-9]{4})$#"],
             "email" => ["required", "max:100", "email"],
             "message" => ["required", "max:1000"]
         ];
@@ -42,5 +42,27 @@ class MessageRequest extends FormRequest
         throw new HttpResponseException(
             response()->json($Response, 400)
         );
+    }
+
+    public function messages()
+    {
+        return [
+            "name.required" => "Precisa digitar um Nome",
+            "name.max" => "O Nome - Máximo 50 caracteres",
+
+            "lastName.required" => "Precisa digitar um Sobrenome",
+            "lastName.max" => "O Sobrenome - Máximo 100 caracteres",
+
+            "telephone.required" => "Precisa digitar um Telefone",
+            "telephone.max" => "O Telefone deve manter os seguintes formatos: +55 (55) xxxxx - xxxx ou +598 xxxxx - xxxx",
+            "telephone.regex" => "O Telefone deve manter os seguintes formatos: +55 (55) xxxxx - xxxx ou +598 xxxxx - xxxx",
+
+            "email.required" => "Precisa digitar um Email",
+            "email.max" => "O Email - Máximo 100 caracteres",
+            "email.email" => "O Email deve ser um email válido",
+
+            "message.required" => "Precisa digitar um Mensagem",
+            "message.max" => "A Mensagem - Máximo 1000 caracteres"
+        ];
     }
 }
