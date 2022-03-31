4a2fc5e346d9d49c4a065d613ff1fb64736cf9b3
DinosDev <erick@bilhalba.com.br>
2022-03-31 00:56:41 -0300

    feat(Back End): Create a New Message Mailable

---
+++ backend/app/Mail/NewMessage.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Mail;
+
+class NewMessage extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public string $Name;
+    public string $LastName;
+    public string $Telephone;
+    public string $Email;
+    public string $Message;
+
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($Name, $LastName, $Telephone, $Email, $Message)
+    {
+        $this->Name = $Name;
+        $this->LastName = $LastName;
+        $this->Telephone = $Telephone;
+        $this->Email = $Email;
+        $this->Message = $Message;
+    }
+
+    static public function SendMail($Values)
+    {
+        $Mail = new NewMessage(
+            $Values["name"],
+            $Values["lastName"],
+            $Values["telephone"],
+            $Values["email"],
+            $Values["message"],
+        );
+
+        $Mail->subject("Nova Mensagem");
+
+        $User = (object)[
+            "email" => $Values["email"],
+            "name" => $Values["name"]
+        ];
+
+        return Mail::to($User)->send($Mail);
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->markdown('mail.NewMessage');
+    }
+}
