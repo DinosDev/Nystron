5cfb75506e3f0adbd24f33da24605bcc698b0aa7
DinosDev <erick@bilhalba.com.br>
2022-03-31 00:56:12 -0300

    feat(Back End): Create a New Message Mail Template

---
+++ backend/resources/views/mail/NewMessage.blade.php
@@ -0,0 +1,14 @@
+@component('mail::message')
+
+# Uma Nova Mensagem Foi Enviada
+
+Nome: **{{$Name}}**
+
+Sobrenome: **{{$LastName}}**
+
+Telefone: **{{$Telephone}}**
+
+Email: **{{$Email}}**
+
+Mensagem:  **{{$Message}}**
+@endcomponent
