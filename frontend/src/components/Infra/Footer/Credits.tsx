85093d9705c8c0ceed9082534a1d211294d91fc9
DinosDev <erick@bilhalba.com.br>
2022-05-15 01:37:12 -0300

    feat: Update Credits URL

--- frontend/src/components/Infra/Footer/Credits.tsx
+++ frontend/src/components/Infra/Footer/Credits.tsx
@@ -5,7 +5,7 @@ const CreditsContainer = styled.div``
 export default function Credits() {
     return (
         <CreditsContainer>
-            <p>Developed by <a href="https://github.com/DinosDev">Erick</a></p>
+            <p>Developed by <a href="https://github.com/TheDevick">Erick</a></p>
             <p>Website in Development</p>
         </CreditsContainer>
     )
